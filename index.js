c53de23c516b5a88f7bf39ec2922b232d6e72061
astral4 <88992929+astral4@users.noreply.github.com>
2024-04-29 09:13:33 -0400

    make sure document body exists before processing webpage

--- index.js
+++ index.js
@@ -2,52 +2,56 @@ function capitalize(text) {
     return text.charAt(0).toUpperCase() + text.substring(1);
 }
 
-let terms = {
-    "AI": "Fumo",
-    "artificial intelligence": "Fumo",
-    "Artificial Intelligence": "Fumo",
-    "an AI": "a Fumo",
-    "an artificial intelligence": "a Fumo",
-    "an Artificial Intelligence": "a Fumo",
-    "ML": "Fumo",
-    "machine learning": "Fumo",
-    "Machine Learning": "Fumo",
-    "an ML": "a Fumo",
-    "transformer": "transfumo",
-    "LLM": "LLF",
-    "language model": "language Fumo",
-    "Language Model": "Language Fumo",
-    "chatbot": "fumobot",
-    "ChatGPT": "ChatFumo",
-    "OpenAI": "FumoCorp",
-    "Anthropic": "FumoCo",
-    "AGI": "Cirno",
-    "artificial general intelligence": "Cirno",
-};
+const { body } = document;
 
-// Add matches for capitalized and pluralized versions of terms
-for (const [term, replacement] of Object.entries(terms)) {
-    terms[term + "s"] = replacement + "s";
+if (body) {
+    let terms = {
+        "AI": "Fumo",
+        "artificial intelligence": "Fumo",
+        "Artificial Intelligence": "Fumo",
+        "an AI": "a Fumo",
+        "an artificial intelligence": "a Fumo",
+        "an Artificial Intelligence": "a Fumo",
+        "ML": "Fumo",
+        "machine learning": "Fumo",
+        "Machine Learning": "Fumo",
+        "an ML": "a Fumo",
+        "transformer": "transfumo",
+        "LLM": "LLF",
+        "language model": "language Fumo",
+        "Language Model": "Language Fumo",
+        "chatbot": "fumobot",
+        "ChatGPT": "ChatFumo",
+        "OpenAI": "FumoCorp",
+        "Anthropic": "FumoCo",
+        "AGI": "Cirno",
+        "artificial general intelligence": "Cirno",
+    };
 
-    const capTerm = capitalize(term);
+    // Add matches for capitalized and pluralized versions of terms
+    for (const [term, replacement] of Object.entries(terms)) {
+        terms[term + "s"] = replacement + "s";
 
-    if (term !== capTerm) {
-        const capReplacement = capitalize(replacement);
+        const capTerm = capitalize(term);
 
-        terms[capTerm] = capReplacement;
-        terms[capTerm + "s"] = capReplacement + "s";
+        if (term !== capTerm) {
+            const capReplacement = capitalize(replacement);
+
+            terms[capTerm] = capReplacement;
+            terms[capTerm + "s"] = capReplacement + "s";
+        }
     }
-}
 
-const regex = new RegExp(Object.keys(terms).join("|"), "g");
+    const regex = new RegExp(Object.keys(terms).join("|"), "g");
 
-const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+    const walker = document.createTreeWalker(body, NodeFilter.SHOW_TEXT);
 
-while (walker.nextNode()) {
-    const node = walker.currentNode;
+    while (walker.nextNode()) {
+        const node = walker.currentNode;
 
-    // Don't substitute text in input boxes
-    if (!node.parentNode.isContentEditable) {
-        node.textContent = node.textContent.replace(regex, (match) => terms[match]);
+        // Don't substitute text in input boxes
+        if (!node.parentNode.isContentEditable) {
+            node.textContent = node.textContent.replace(regex, (match) => terms[match]);
+        }
     }
 }
